4e6b0c5c7d51e4f61066a074f3e294ede005973d
imene <imene.bencheikh@ieee.org>
2023-06-24 12:55:18 +0100

    updated file

---
+++ router/ticket.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const Ticket = require('../model/ticket');
+
+// Route pour créer un ticket
+router.post('/tickets', (req, res) => {
+  const newTicket = new Ticket(req.body);
+  newTicket.save((err, ticket) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(201).json(ticket);
+    }
+  });
+});
+
+// Route pour obtenir tous les tickets
+router.get('/tickets', (req, res) => {
+  Ticket.find({}, (err, tickets) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(tickets);
+    }
+  });
+});
+
+// Route pour obtenir un ticket spécifique
+router.get('/tickets/:id', (req, res) => {
+  Ticket.findById(req.params.id, (err, ticket) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      if (ticket) {
+        res.json(ticket);
+      } else {
+        res.status(404).json({ message: 'Ticket non trouvé' });
+      }
+    }
+  });
+});
+
+// Route pour mettre à jour un ticket
+router.put('/tickets/:id', (req, res) => {
+  Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, ticket) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        if (ticket) {
+          res.json(ticket);
+        } else {
+          res.status(404).json({ message: 'Ticket non trouvé' });
+        }
+      }
+    }
+  );
+});
+
+// Route pour supprimer un ticket
+router.delete('/tickets/:id', (req, res) => {
+  Ticket.findByIdAndRemove(req.params.id, (err, ticket) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      if (ticket) {
+        res.json({ message: 'Ticket supprimé avec succès' });
+      } else {
+        res.status(404).json({ message: 'Ticket non trouvé' });
+      }
+    }
+  });
+});
+
+module.exports = router;
