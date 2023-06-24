4ab7e3ae8cffe2e063578a799e0bde43e1128e77
imene <imene.bencheikh@ieee.org>
2023-06-24 13:09:37 +0100

    commit index file

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
 const app = express();
-const port = 3000;
+
+
+
+
 const userRouter = require("./router/user");
+const ticketRoutes = require('./router/ticket');
+const ticketTypeRoutes = require('./router/ticketType');
+
+app.use(bodyParser.json());
+
+app.use('/api', ticketRoutes);
+app.use('/api', ticketTypeRoutes);
 
 app.use(express.json());
 app.use("/user", userRouter);
+
+const mongoDBURL = 'mongodb://localhost:27017/bibconnect';
+mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur de connexion à la base de données'));
+db.once('open', () => {
+  console.log('Connecté à la base de données');
+});
+
+const port = 3000;
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
