9a71bb430ea7ee069b0169d8c9ccba2fa741cfcd
7asyou <7asyou@gmail.com>
2023-07-17 14:11:05 +0100

    merging bugs

--- models/user.js
+++ models/user.js
@@ -4,39 +4,39 @@ const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
-		unique : true,
-
-	firstName: {
-		type: String,
-		required: true,
-	},
-	lastName: {
-		type: String,
-		required: true,
-	},
-	birthday: {
-		type: Date,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
 		unique: true,
-	},
-	password: {
-		type: String,
-	},
 
-	role: {
-		type: String,
-		enum: ["admin", "reader", "author"],
-		required: true,
-	},
-	books: {
-		type: [mongoose.Types.ObjectId],
-		ref: "Book",
-	},
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		birthday: {
+			type: Date,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+		},
 
+		role: {
+			type: String,
+			enum: ["admin", "reader", "author"],
+			required: true,
+		},
+		books: {
+			type: [mongoose.Types.ObjectId],
+			ref: "Book",
+		},
+	},
 });
 
 const User = mongoose.model("User", userSchema);
